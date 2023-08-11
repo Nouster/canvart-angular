ea369ed160048d1edef64f9b56a37f709c211ff7
Nouster <djebali.mohamed88@gmail.com>
2023-08-11 12:18:18 +0200

    button effects

--- src/app/home/button-effects.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { ButtonEffectsDirective } from './button-effects.directive';
-
-describe('ButtonEffectsDirective', () => {
-  it('should create an instance', () => {
-    const directive = new ButtonEffectsDirective();
-    expect(directive).toBeTruthy();
-  });
-});
--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -2,10 +2,9 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BannerComponent } from './banner/banner.component';
 import { HomeComponent } from './home/home.component';
-import { ButtonEffectsDirective } from './button-effects.directive';
 
 @NgModule({
-  declarations: [BannerComponent, HomeComponent, ButtonEffectsDirective],
+  declarations: [BannerComponent, HomeComponent],
   imports: [CommonModule],
   exports: [HomeComponent],
 })
