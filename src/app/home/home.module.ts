0f73f0fa7506b8cf8493a3ce31121fe93c19600b
Nouster <djebali.mohamed88@gmail.com>
2023-08-11 12:23:42 +0200

    first section component for homepage

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BannerComponent } from './banner/banner.component';
 import { HomeComponent } from './home/home.component';
+import { FirstSectionComponent } from './first-section/first-section.component';
 
 @NgModule({
-  declarations: [BannerComponent, HomeComponent],
+  declarations: [BannerComponent, HomeComponent, FirstSectionComponent],
   imports: [CommonModule],
   exports: [HomeComponent],
 })
