0a01a5bae48c8f2c4685f16028ac2fcff901cfad
Nouster <djebali.mohamed88@gmail.com>
2023-08-14 10:25:20 +0200

    first section home

--- src/app/home/first-section/first-section.component.ts
+++ src/app/home/first-section/first-section.component.ts
@@ -3,8 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-first-section',
   templateUrl: './first-section.component.html',
-  styleUrls: ['./first-section.component.css']
+  styleUrls: ['./first-section.component.css'],
 })
-export class FirstSectionComponent {
-
-}
+export class FirstSectionComponent {}
--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 import { BannerComponent } from './banner/banner.component';
 import { HomeComponent } from './home/home.component';
 import { FirstSectionComponent } from './first-section/first-section.component';
+import { NgxTypedJsModule } from 'ngx-typed-js';
 
 @NgModule({
   declarations: [BannerComponent, HomeComponent, FirstSectionComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, NgxTypedJsModule],
   exports: [HomeComponent],
 })
 export class HomeModule {}
