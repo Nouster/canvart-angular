c2459d51b79a743d152cdf96858b625a89e748cc
Nouster <djebali.mohamed88@gmail.com>
2023-08-24 12:54:28 +0200

    fixed getUserData and update model to get nfts

---
+++ src/app/my-profile/my-gallery/my-gallery.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-my-gallery',
+  templateUrl: './my-gallery.component.html',
+  styleUrls: ['./my-gallery.component.css']
+})
+export class MyGalleryComponent {
+
+}
---
+++ src/app/my-profile/my-profile.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MyGalleryComponent } from './my-gallery/my-gallery.component';
+
+
+
+@NgModule({
+  declarations: [
+    MyGalleryComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class MyProfileModule { }
