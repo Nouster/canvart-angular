42b6035e968e5fb5c83e0b85aa0980c9a7155080
Nouster <djebali.mohamed88@gmail.com>
2023-08-28 14:00:46 +0200

    edit form component -> next on edit_form branch

--- src/app/my-profile/edit/edit.component.ts
+++ src/app/my-profile/edit/edit.component.ts
@@ -1,10 +1,55 @@
 import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { UserService } from 'src/app/service/Canvart/user.service';
 
 @Component({
   selector: 'app-edit',
   templateUrl: './edit.component.html',
-  styleUrls: ['./edit.component.css']
+  styleUrls: ['./edit.component.css'],
 })
 export class EditComponent {
+  editForm = this.fb.group({
+    firstName: ['', Validators.required],
+    lastName: ['', Validators.required],
+    email: ['', Validators.email],
+    gender: ['', Validators.required],
+    password: ['', Validators.required],
+    birthDate: ['', Validators.required],
+    address: this.fb.group({
+      street: ['', Validators.required],
+      zipCode: ['', Validators.required],
+      city: ['', Validators.required],
+      country: ['', Validators.required],
+    }),
+  });
 
+  constructor(
+    private userService: UserService,
+    private fb: FormBuilder,
+    private toast: ToastrService,
+    private router: Router
+  ) {}
+
+  submit(): void {
+    if (this.editForm.invalid) {
+      this.toast.error('Please fill in all required fields.');
+      return;
+    }
+    this.userService.addUser(this.editForm).subscribe(
+      () => {
+        this.toast.success('User added successfully');
+        this.router.navigate(['/login']);
+      },
+      (err) => {
+        if (
+          err.error &&
+          err.error.message &&
+          err.error.message.includes('duplicate')
+        )
+          this.toast.error('Email already in use');
+      }
+    );
+  }
 }
--- src/app/my-profile/my-profile.module.ts
+++ src/app/my-profile/my-profile.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 import { MyGalleryComponent } from './my-gallery/my-gallery.component';
 import { NgxTypedJsModule } from 'ngx-typed-js';
 import { EditComponent } from './edit/edit.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [MyGalleryComponent, EditComponent],
-  imports: [CommonModule, NgxTypedJsModule],
+  imports: [CommonModule, NgxTypedJsModule, ReactiveFormsModule],
 })
 export class MyProfileModule {}
